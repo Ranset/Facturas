6b1baf111d6d140a18d8f76518f4d6e9ee1bfb51
Ranset Fleites <ransetfleites0@gmail.com>
2026-02-10 00:32:36 -0500

    Refactor Vendedor page to include additional fields and improve layout

--- pages/vendedor.py
+++ pages/vendedor.py
@@ -21,74 +21,122 @@ class Vendedor(ft.Container):
             size= 35,
             weight= ft.FontWeight.BOLD,
             )
+        
+        datos_vendedor = (
+            "Yadira Hernández Herrera",
+            "88042108175",
+            "+5354132764",
+            "Calle Serafina #12 e/ Ulacia y Castillo, Reparto Juanelo, San Miguel del Padrón. La Habana, Cuba. C.P. 11000.",
+            "0598770015216512",
+            "9212959871557908",
+            "0598770015216512",
+            "9212959871557908",
+            "yadirahernandez0421@gmail.com"
+            )
 
         txt_nombre = ft.TextField(
+                            value= datos_vendedor[0],
                             label="Nombre Comercial",
-                            
-                            # expand= True,
+                            height= inputs_height,
+                            bgcolor= inputs_bgcolor,
+                            border_color= inputs_border_color,
                             )
         
         txt_nit = ft.TextField(
+                            value= datos_vendedor[1],
                             label="NIT",
-                            
-                            # expand= True,
+                            height= inputs_height,
+                            bgcolor= inputs_bgcolor,
+                            border_color= inputs_border_color,
+                            expand= True,
                             )
         
         txt_telefono = ft.TextField(
+                            value= datos_vendedor[2],
                             label="Teléfono",
-                            
-                            # expand= True,
+                            height= inputs_height,
+                            bgcolor= inputs_bgcolor,
+                            border_color= inputs_border_color,
+                            expand= True,
                             )
         
         txt_email = ft.TextField(
+                            value= datos_vendedor[8],
                             label="Email",
-                            
+                            height= inputs_height,
+                            bgcolor= inputs_bgcolor,
+                            border_color= inputs_border_color,
+                            expand= True
                             )
         
         txt_domicilio = ft.TextField(
+                            value= datos_vendedor[3],
                             label="Domcilio",
-                            
-                            # expand= True,
+                            height= inputs_height,
+                            bgcolor= inputs_bgcolor,
+                            border_color= inputs_border_color,
                             )
         
         txt_cta_cup = ft.TextField(
+                            value= datos_vendedor[4],
                             label="# de Cuenta CUP",
-                            
-                            # expand= True,
+                            height= inputs_height,
+                            bgcolor= inputs_bgcolor,
+                            border_color= inputs_border_color,
+                            expand= True,
                             )
         
         txt_cta_mlc = ft.TextField(
+                            value= datos_vendedor[5],
                             label="# de Cuenta MLC",
-                            
-                            # expand= True,
+                            height= inputs_height,
+                            bgcolor= inputs_bgcolor,
+                            border_color= inputs_border_color,
+                            expand= True,
                             )
         
         txt_tarjeta_cup = ft.TextField(
-                            label="# de Cuenta CUP",
-                            
-                            # expand= True,
+                            value= datos_vendedor[6],
+                            label="# de Tarjeta CUP",
+                            height= inputs_height,
+                            bgcolor= inputs_bgcolor,
+                            border_color= inputs_border_color,
+                            expand= True,
                             )
         
         txt_tarjeta_mlc = ft.TextField(
-                            label="# de Cuenta MLC",
-                            
-                            # expand= True,
+                            value= datos_vendedor[7],
+                            label="# de Tarjeta MLC",
+                            height= inputs_height,
+                            bgcolor= inputs_bgcolor,
+                            border_color= inputs_border_color,
+                            expand= True,
                             )
+        
+        def click_guardar(e):
+            from router import show_view
+            show_view(page, States._configuracion_location)
         
         btn_guardar = ft.ElevatedButton(
             "Guardar",
             style= ft.ButtonStyle(bgcolor= "#2c78d0", color= "white", shape= ft.RoundedRectangleBorder(radius= 5)),
             width= 120,
+            height= inputs_height,
+            expand= True,
+            on_click= click_guardar
         )
 
         def click_cancelar(e):
-            pass
+            from router import show_view
+            show_view(page, States._configuracion_location)
         
         btn_cancelar = ft.OutlinedButton(
             "Canelar",
             style= ft.ButtonStyle(side= ft.BorderSide(1, "#2c78d0"), color= "#2c78d0", shape= ft.RoundedRectangleBorder(radius= 5)),
             width= 120,
-            on_click= click_cancelar
+            height= inputs_height,
+            on_click= click_cancelar,
+            expand= True
         )
 
         ## Widgets objects>
@@ -116,21 +164,28 @@ class Vendedor(ft.Container):
                     txt_nit,
                     txt_telefono,
                     txt_email
-                ]),
+                ], expand= True),
                 txt_domicilio,
                 ft.Row([
                     txt_cta_cup,
-                    txt_cta_mlc
+                    txt_cta_mlc,
+                    btn_guardar
                 ]),
                 ft.Row([
                     txt_tarjeta_cup,
-                    txt_tarjeta_mlc
+                    txt_tarjeta_mlc,
+                    btn_cancelar
                 ]),
             ]
         )
 
+        contenedor_formulario = ft.Container(
+            content= column_formulario,
+            width= 600,
+        )
+
         columna_menu = ft.Column(controls= [Menu().Crear()])
-        column2 = ft.Column(controls=[contenedor1], expand= True)
+        column2 = ft.Column(controls=[contenedor1, contenedor_formulario], expand= True, horizontal_alignment= "center")
 
         Row_generar = ft.Row(controls=[columna_menu, column2], alignment= ft.MainAxisAlignment.CENTER, spacing= 0, expand= True)
 
