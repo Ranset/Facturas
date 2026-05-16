cd5eb7579f4ac65450755a9833b24362875343a4
Ranset <ransetfleites0@gmail.com>
2026-05-16 18:00:38 -0500

    correct version number

--- pages/acerca.py
+++ pages/acerca.py
@@ -21,7 +21,7 @@ class Acerca(ft.Container):
             )
         
         txt_version = ft.Text(
-            "Versión 1.1.1",
+            "Versión 1.2.0",
             size= 20,
             )
         
